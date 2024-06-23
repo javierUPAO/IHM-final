73ea26e9ac69e11e5996203b13c5fab133ce908b
Dylan Acevedo <127446864+eddyacv@users.noreply.github.com>
2024-06-23 18:29:11 +0000

    creando componente categorias

---
+++ final/src/components/categorias.js
@@ -0,0 +1,45 @@
+function Categoria(){
+    return (
+        <div>
+            <div className="p20">
+        <div className="row">
+            <div className="col-md-12 d-flex justify-content-center">
+                <h2 className="txt-c">Categorías de Productos</h2>
+            </div>
+        </div>
+        
+
+        
+        <div className="container-fluid scroll">
+            <div className="item c2">
+                <img className="img-fluid img-zoom" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.39-pm-1.jpeg" alt="imagen"/>
+                <p>Cortinas para cocina</p>
+            </div>
+
+            <div className="item c2">
+                <img className="img-fluid img-zoom" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.30.01-pm-1.jpeg?w=768" alt="imagen"/>
+                <p>Cortinas con riel</p>
+            </div>
+
+            <div className="item c2">
+                <img className="img-fluid img-zoom" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-1.54.45-pm.jpeg" alt="imagen"/>
+                <p>Forros para muebles y sillas</p>
+            </div>
+
+            <div className="item c2">
+                <img className="img-fluid img-zoom" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-1.49.40-pm.jpeg" alt="imagen"/>
+                <p>Rollers y Persianas</p>
+            </div>
+
+            <div className="item c2">
+                <img className="img-fluid img-zoom" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.40.07-pm-1.jpeg?w=768" alt="imagen"/>
+                <p>Cortinas con Tubo de Acero</p>
+            </div>
+
+            
+        </div>
+        </div>
+    </div>
+    );
+}
+export default Categoria;
