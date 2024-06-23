e3c0a1ac7daa5f36abca8bfd38322856734f144a
Dylan Acevedo <127446864+eddyacv@users.noreply.github.com>
2024-06-23 18:26:29 +0000

    cambiando el componente carrusel

--- final/src/components/carrusel.js
+++ final/src/components/carrusel.js
@@ -1,29 +1,40 @@
 import Carousel from 'react-bootstrap/Carousel';
 import { useNavigate } from "react-router-dom";
-import productos from '../datos.json';
+import productos from "../json/carrusel.json";
+
 
 const Carrusel= () => {
-  const navigate = useNavigate();
-
-  const handleClick = (slug) => {
-      navigate(slug);
-  }
-  return(  
+  return (
     <Carousel>
-        {
-          productos.map(it =>
-           {
-             return (
-                  <Carousel.Item onClick={(e) => {e.preventDefault(); handleClick(it.slug) }}>
-                  <img className="img-fluid w-30" src={it.url}/>
-                  </Carousel.Item>     
-                    );
-   
-            })
+      {
+        productos.map(it =>
+         {
+           return (
+                <Carousel.Item>
+                  <div className="d-flex justify-content-center mxh-70 align-items-center position-relative hover-div">
 
-        }
+                    <img className="img-fluid" src={it.src}/>
+                    <div className="position-absolute text-center w-100 overlay align-content-center z1">
+                      <span className='z2 text-center title'>Gino's <br/><br/></span>
+                      <span className='z2 text-center'>Sumergete en nuestro mundo de inspiración y creatividad. <br />
+                            ¡Bienvenido a Gino-s donde cada espacio cuenta una historia <br />
+                            única de estilo y belleza!</span>
+                      
+                    </div>
+                  </div>
+                
+                </Carousel.Item>     
+                  );
+ 
+          })
+
+      }
+  
     </Carousel>
-    );
+  );
+
+  
+    
 }
 
 export default Carrusel;
