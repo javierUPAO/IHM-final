6ad4f0f0ce945e9d00be3f6e69ff7385f3847a69
Dylan Acevedo <127446864+eddyacv@users.noreply.github.com>
2024-06-23 18:31:27 +0000

    Implementando los componentes creados anteriormente en inicio

--- final/src/views/home.js
+++ final/src/views/home.js
@@ -3,52 +3,42 @@ import { Link } from 'react-router-dom';
 import "../styles/style.css"
 function Home() {
     return (
-        <div className="container n-p I">
-            <div className="row vh-70 n-m">
-                <div className="col-md-7 inicio">
-                    <div className="row">
-                        <h1>Decoraciones textiles Gino's</h1>
+        <div>
+            <Carrusel/>
+            <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div className="col-md-5">
+                        <div className="container-1">
+
+                            <div className="row c1">
+                                <img className="img-fluid" src="https://images.pexels.com/photos/813691/pexels-photo-813691.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Responsive Image"></img>
+                            </div>
+                            <div className="row">
+                                <p>Direccion:  Av. Húsares de Junín 801(La Libertad, Trujillo)</p>
+                            </div>
+
+                        </div>
                     </div>
-                    <div className="row">
-                        <p>
-                            Sumergete en nuestro mundo de inspiración y creatividad. <br />
-                            ¡Bienvenido a Gino-s donde cada espacio cuenta una historia <br />
-                            única de estilo y belleza!
-                        </p>
+
+                    <div className="col-md-5">
+                        <div className="container-1">
+
+                            <div className="row c1">
+                                <img className="img-fluid" src="https://i.ibb.co/dPQvGL9/Contacto.png" alt="Responsive Image"></img>
+                            </div>
+                            <div className="row">
+                                <p>Contacto:949314078 </p>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-5 inicio">
-                    <p className="a-m">Imagen</p>
                 </div>
             </div>
 
-            <div className="row n-m">
-                <div className="row vh-60 n-p n-m">
-                    <div className="col-md-6 inicio n-p">
-                        <div className="row titulo">
-                            <h2>Encuentranos</h2>
-                        </div>
-                        <div className="row img h-st">
-                            <p>imagen</p>
-                        </div>
-                        <div className="row boton-1">
-                            <a href="#">Ubicanos</a>
-                        </div>
-                    </div>
-                    <div className="col-md-6 inicio n-p">
-                        <div className="row titulo">
-                            <h2>Contactanos</h2>
-                        </div>
-                        <div className="row telefonos h-st">
-                            <p>98753447 <br />987646789 <br /> 098646747</p>
-                        </div>
-                        <div className="row boton-1">
-                            <a href="#">Contactanos</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <Trabajos/>
+            <Categoria/>
+            
+       </div>
     );
 }
 
