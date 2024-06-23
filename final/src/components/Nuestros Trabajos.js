573905dea346b90d115e588e5d68e3f20ee20ee1
Dylan Acevedo <127446864+eddyacv@users.noreply.github.com>
2024-06-23 18:28:23 +0000

    creando componente nuestros trabajos

---
+++ final/src/components/Nuestros Trabajos.js
@@ -0,0 +1,49 @@
+import { ListGroupItem } from "react-bootstrap";
+
+function Trabajos() {
+  return (
+    <div>
+         <div className="cp">
+        <div className="row">
+            <div className="col-md-12 d-flex justify-content-center">
+                <h2 className="txt-c">Nuestros Trabajos</h2>
+            </div>
+        </div>
+       
+
+        
+        <div className="container-fluid scroll">
+            <div className="item">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+
+            <div className="item">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+
+            <div className="item">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+
+            <div className="item">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+
+            <div className="item">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+
+            <div className="item">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+
+            <div className="item last">
+                <img className="img-fluid" src="https://decortextilginos.wordpress.com/wp-content/uploads/2022/08/whatsapp-image-2022-08-17-at-2.49.42-pm-2.jpeg" alt="imagen"/>
+            </div>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default Trabajos;
